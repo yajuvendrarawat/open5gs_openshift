39e791faba6e67471b1d3e5407a8164c0b903b7c
yajuvendrarawat <43192654+yajuvendrarawat@users.noreply.github.com>
2023-06-08 14:37:48 +0800

    Update amf_init.sh

--- amf/amf_init.sh
+++ amf/amf_init.sh
@@ -26,12 +26,12 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-cp /mnt/amf/amf.yaml install/etc/open5gs
-sed -i 's|AMF_IP|'$AMF_IP'|g' install/etc/open5gs/amf.yaml
-sed -i 's|SCP_IP|'$SCP_IP'|g' install/etc/open5gs/amf.yaml
-sed -i 's|NRF_IP|'$NRF_IP'|g' install/etc/open5gs/amf.yaml
-sed -i 's|MNC|'$MNC'|g' install/etc/open5gs/amf.yaml
-sed -i 's|MCC|'$MCC'|g' install/etc/open5gs/amf.yaml
+cp /mnt/amf.yaml /open5gs/install/etc/open5gs
+sed -i 's|AMF_IP|'$AMF_IP'|g' /open5gs/install/etc/open5gs/amf.yaml
+sed -i 's|SCP_IP|'$SCP_IP'|g' /open5gs/install/etc/open5gs/amf.yaml
+sed -i 's|NRF_IP|'$NRF_IP'|g' /open5gs/install/etc/open5gs/amf.yaml
+sed -i 's|MNC|'$MNC'|g' /open5gs/install/etc/open5gs/amf.yaml
+sed -i 's|MCC|'$MCC'|g' /open5gs/install/etc/open5gs/amf.yaml
 
 # Sync docker time
 #ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
